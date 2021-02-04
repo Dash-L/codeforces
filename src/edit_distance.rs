5ca90702dd509057c9d15579c0b662eec53b35ff
Dashiell Elliott <dashiell.elliott@yahoo.com>
2021-02-04 09:26:25 -0500

    complete edit distance but it's O(nm) which is too slow

--- src/edit_distance.rs
+++ src/edit_distance.rs
@@ -25,42 +25,31 @@ macro_rules! parse_input {
         parse_input!().split(' ').map(|v| v.parse::<$ty>().unwrap()).collect::<Vec<$ty>>()
     }
 }
-/*
-fn dist(w1: &str, w2: &str, a: usize, b: usize, dists: &mut std::collections::HashMap<(usize, usize), usize>) -> usize {
-    if a == 0 || b == 0 {
-        return a + b;
-    }
-    if w1[0..a] == w2[0..b] {
-        return 0;
-    }
-    if let Some(ans) = dists.get(&(a, b)) {
-        return *ans;
-    }
-
-    dists.insert((a, b), 0);
-    let insert = dist(w1, w2, a - 1, b, dists) + 1;
-    let delete = dist(w1, w2, a, b - 1, dists) + 1;
-    let modify = dist(w1, w2, a - 1, b - 1, dists) + if w1[a-1..a] == w2[b-1..b] { 0 } else { 1 };
-
-    dists.insert((a, b), usize::min(usize::min(insert, delete), modify));
-    dists[&(a, b)]
-}
-*/
-use std::collections::HashMap;
 
 fn main() {
     let w1 = parse_input!();
     let w2 = parse_input!();
-    let mut dists: HashMap<(usize, usize), usize> = HashMap::new();
-    dists.insert((0, 0), 0);
+    let n = w1.len();
+    let m = w2.len();
+    let mut dists: Vec<Vec<usize>> = (0..=n).map(|_| [0].repeat(m + 1)).collect();
     let mut a = 0;
-    let mut b = 0;
-    while a <= w1.len() {
-        while b <= w2.len() {
-            dists.insert((a, b), *[dists[&(a - 1, b)] + 1, dists[&(a, b - 1)] + 1, dists[&(a - 1, b - 1)] + if w1[a-1..a] == w2[b-1..b] { 0 } else { 1 }].iter().min().unwrap());
+    let mut b;
+    while a <= n {
+        b = 0;
+        while b <= m {
+            if a == 0 || b == 0 {
+                dists[a][b] = a + b;
+                b += 1;
+                continue;
+            }
+            dists[a][b] = *[
+                dists[a - 1][b] + 1,
+                dists[a][b - 1] + 1,
+                dists[a - 1][b - 1] + if w1.chars().nth(a - 1).unwrap() == w2.chars().nth(b - 1).unwrap() { 0 } else { 1 },
+            ].iter().min().unwrap();
             b += 1;
         }
         a += 1;
     }
-    println!("{}", dists[&(w1.len(), w2.len())]);
+    println!("{}", dists[n][m]);
 }
